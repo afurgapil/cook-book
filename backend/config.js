ad8266fea2a6cacb76797f22c621b69c778bb130
Gapil <gapilsoft@gmail.com>
2023-07-26 19:44:25 +0300

    added instructions

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,10 @@
+//TODOS
+//Create a .env file and configure this variables:
+//MONGO_DB=mongodb connection string
+//UNSPLASH_ACCESS
+//UNSPLASH_SECRET
+//MAIL_PASSWORD=nodemailer connection password
+
 const PORT = 8070;
 const TOKEN_EXPIRATION_MINUTES = 5 * 60 * 1000;
 const APP_URL = "http://54.208.30.54:36345";
--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,3 +1,6 @@
+//TODOS
+//Create a .env file and configure this variables:
+//VITE_FORMSPREE_KEY=formsprree form key
 const API_URL = "http://localhost:8070";
 // const API_URL = "http://54.208.30.54:8070";
 
